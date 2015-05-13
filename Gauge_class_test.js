69f5a5c88256bdb9b8e39f676972c29f1b14ff03
Justin Barabander <justinbarabander@Justins-MBP.home>
2015-05-13 10:19:30 -0400

    including test files and Raphael has been moved into separate folder

--- Gauge_class_test.js
+++ Gauge_class_test.js
@@ -2,26 +2,27 @@
 function Gauge(){
 
 	setAssumed = function(att, def){
-		att !== 'undefined' ? att : def;
+		return typeof att !== 'undefined' ? att : def;
 	}
 
 	this.init = function(){
 		this.setLocation();
 		this.setTransDur();
 		this.setExtrema();
-		this.setTickerSettings();
+		this.setDefault();
 		this.setGaugeSettings();
+		this.setTickerSettings();
 		this.setSpinnerSettings();
 		this.setPointerSettings();
-		this.setDefault();
-	}
+		this.setValue();
+	} 
 
 	this.setLocation = function(size, startX, startY){
-		this.size = setAssumed(size, 100);
+		this.size = setAssumed(size, 200);
 		this.startX = setAssumed(startX, 0);
 		this.startY = setAssumed(startY, 0);
-		this.cx = startX + size / 2;
-		this.cy = startY + size / 2;
+		this.cx = this.startX + this.size / 2;
+		this.cy = this.startY + this.size / 2;
 	}
 
 	this.setTransDur = function(duration){
@@ -31,44 +32,39 @@ function Gauge(){
 	this.setExtrema = function(min,max){
 		this.min = setAssumed(min, 0);
 		this.max = setAssumed(max, 100);
-		this.range = max - min;
-	}
-
-	this.setTickerSettings = function(majorTickers, minorPerMajor, tickerColor, 
-								      majorTickerWidth, majorTickerLength, 
-								      minorTickerWidth, minorTickerLength){
-		baseSize = this.size * .02;
-		baseMajLength = this.size * .21;
-		baseMinLength = this.size * .07;
-		this.majorTickers = setAssumed(majorTicker, 15);
-		this.minorPerMajor = setAssumed(minorPerMajor, 1);
-		this.tickerColor = setAssumed(tickerColor, "#00FA9A");
-		this.majorTickerWidth = setAssumed(majorTickerWidth, baseSize);
-		this.minorTickerWidth = setAssumed(minorTickerWidth, baseSize);
-		this.majorTickerLength = setAssumed(majorTickerLength, baseMajLength);
-		this.minorTickerLength = setAssumed(minorTickerLength, baseMinLength);
-		this.totalTickers = this.majorTickers + this.majorTickers * this.minorPerMajor - 1;
-		this.tickersPerSect = this.majorTickers + this.minorPerMajor;
-		this.tickerAngle = 240 / totalTickers;
-		this.tickerRadius = this.radius * 0.8;
+		this.range = this.max - this.min;
 	}
 
 	this.setGaugeSettings = function(gaugeColor){
 		this.gaugeColor = setAssumed(gaugeColor, "black");
-		this.radius = size * 0.5;
+		this.radius = this.size * 0.485;
 		//Possibly add angle setter
 	}
 
-	this.setSpinnerSettings = function(spinnerStrokeColor, spinnerFillColor,spinnerRadius){
+	this.setTickerSettings = function(majorTickers, minorPerMajor, tickerColor, majorTickerWidth, majorTickerLength, minorTickerWidth, minorTickerLength){
+		this.majorTickers = setAssumed(majorTickers, 14);
+		this.minorPerMajor = setAssumed(minorPerMajor, 1);
+		this.tickerColor = setAssumed(tickerColor, "#00FA9A");
+		this.majorTickerWidth = setAssumed(majorTickerWidth, this.size * .02);
+		this.minorTickerWidth = setAssumed(minorTickerWidth, this.size * .02);
+		this.majorTickerLength = setAssumed(majorTickerLength, this.size * .12);
+		this.minorTickerLength = setAssumed(minorTickerLength, this.size * .07);
+		this.totalTickers = this.majorTickers * (1 + this.minorPerMajor);
+		this.tickersPerSect = 1 + this.minorPerMajor;
+		this.tickerAngle = 240 / this.totalTickers;
+		this.tickerRadius = this.radius * 0.7;
+	}
+
+	this.setSpinnerSettings = function(spinnerStrokeColor, spinnerFillColor, spinnerRadius){
 		this.spinnerFillColor = setAssumed(spinnerFillColor, "black");
 		this.spinnerStrokeColor = setAssumed(spinnerStrokeColor, "#00FA9A");
-		this.spinnerRadius = setAssumed(spinnerRadius, this.size * 0.15);
+		this.spinnerRadius = setAssumed(spinnerRadius, this.size * 0.13);
 	}
 
 	this.setPointerSettings = function(pointerColor, pointerLength, pointerWidth){
 		this.pointerColor = setAssumed(pointerColor, "#00FA9A");
-		this.pointerLength = setAssumed(pointerLength, this.radius * 0.9);
-		this.pointerWidth = setAssumed(pointerWidth, this.size * .05);
+		this.pointerLength = setAssumed(pointerLength, this.radius * 0.8);
+		this.pointerWidth = setAssumed(pointerWidth, this.size * .1);
 	}
 
 	this.setTitle = function(title){
@@ -76,16 +72,7 @@ function Gauge(){
 	}
 
 	this.setDefault = function(defaultValue){
-		this.defaultValue = setAssumed(defaultValue, 0);
-	}
-
-	this.createDash = function(){
-		this.paper = Raphael(this.startX, this.startY, this.size, this.size);
-		createCircle(this.paper);
-		createMinTickers(this.paper);
-		CreateMajTickers(this.paper);
-		createPointer(this.paper);
-		createSpinner(this.paper);
+		this.defaultValue = setAssumed(defaultValue, this.min);
 	}
 
 	this.refreshDash = function(){
@@ -93,12 +80,12 @@ function Gauge(){
 		this.createDash();
 	}
 
-	createCircle = function(paper){
-		gaugeCircle = paper.circle(this.cx, this.cy, this.radius).attr("fill" : this.gaugeColor);
+	this.createCircle = function(paper){
+		gaugeCircle = paper.circle(this.cx, this.cy, this.radius).attr("fill",this.gaugeColor);
 	}
 
-	createMinTickers = function(paper){
-		for (i = -30; i <= 210; i+=this.tickerAngle){
+	this.createMinTickers = function(paper){
+		for (i = -210; i <= 30; i+=this.tickerAngle){
 			var radiusVector = [this.tickerRadius * math.cos(math.unit(i, 'deg')), this.tickerRadius * math.sin(math.unit(i, 'deg'))];
 			var unitRadiusVector = [math.cos(math.unit(i, 'deg')), math.sin(math.unit(i, 'deg'))];
 
@@ -128,13 +115,16 @@ function Gauge(){
 							 "L" + String(point2[0]) + "," + String(point2[1]) + "L" + String(point3[0]) + "," + String(point3[1]) +
 						 	 "L" + String(point4[0]) + "," + String(point4[1]) + "z";
 			
-			paper.path(pathString).attr("fill":this.tickerColor);
+			paper.path(pathString).attr("fill",this.tickerColor);
 		}
 	}
 
-	createMajTickers = function(paper){
-		for (i = -30; i <= 210; i+=this.tickerAngle*this.tickersPerSect){
-			var radiusVector = [this.tickerRadius * math.cos(math.unit(i, 'deg')), this.tickerRadius * math.sin(math.unit(i, 'deg'))];
+	this.CreateMajTickers = function(paper){
+		for (i = -210; i <= 30; i+=this.tickerAngle * this.tickersPerSect){
+			var radiusVector = [
+					this.tickerRadius * math.cos(math.unit(i, 'deg')),
+			 		this.tickerRadius * math.sin(math.unit(i, 'deg'))
+			 	];
 			var unitRadiusVector = [math.cos(math.unit(i, 'deg')), math.sin(math.unit(i, 'deg'))];
 
 			var point0 = [this.cx + radiusVector[0], this.cy + radiusVector[1]];
@@ -163,16 +153,17 @@ function Gauge(){
 						 	 "L" + String(point2[0]) + "," + String(point2[1]) + "L" + String(point3[0]) + "," + String(point3[1]) +
 						 	 "L" + String(point4[0]) + "," + String(point4[1]) + "z";
 			
-			paper.path(pathString).attr("fill" : this.tickerColor);
+			paper.path(pathString).attr("fill",this.tickerColor);
 		}
 	}
 
-	createPointer = function(paper){
-		var defaultAngle = (this.defaultValue - this.min) * (240 / this.range) - 30; 
+	this.createPointer = function(paper){
+		var defaultAngle = (this.defaultValue - this.min) * (240 / this.range) - 210;
 		var radiusVector = [
-			this.pointerLength * math.cos(math.unit(defaultAngle, 'deg')), 
-			this.pointerLength * math.sin(math.unit(defaultAngle, 'deg'))
-		];
+				this.pointerLength * math.cos(math.unit(defaultAngle, 'deg')), 
+				this.pointerLength * math.sin(math.unit(defaultAngle, 'deg'))
+			];
+
 		var unitRadiusVector = [math.cos(math.unit(defaultAngle, 'deg')), math.sin(math.unit(defaultAngle, 'deg'))];
 		
 		var point0 = [this.cx, this.cy];
@@ -193,24 +184,33 @@ function Gauge(){
 			];
 		
 		var pointerString = "M" + String(point0[0]) + "," + String(point0[1]) + "L" + String(point1[0]) + "," + String(point1[1]) +
-							"L" + String(point2[0]) + "," + String(point2[0]) + "L" + String(point3[0]) + "," + String(point3[1]) +
+							"L" + String(point2[0]) + "," + String(point2[1]) + "L" + String(point3[0]) + "," + String(point3[1]) +
 							"z";
 
-		this.pointer = paper.path(pointerString).attr({fill : this.pointerColor});
+		this.pointer = paper.path(pointerString).attr("fill", this.pointerColor);
 	}
 
-	createSpinner = function(paper){
-		var spinner = paper.circle(this.cx, this.cy, this.spinnerRadius).attr{fill:this.spinnerFillColor, stroke: this.spinnerStrokeColor};
+	this.createSpinner = function(paper){
+		var spinner = paper.circle(this.cx, this.cy, this.spinnerRadius).attr({fill : this.spinnerFillColor, stroke : this.spinnerStrokeColor});
 	}
 
-	setValue = function(data){
+	this.createDash = function(){
+		this.paper = Raphael(this.startX, this.startY, this.size, this.size);
+		this.createCircle(this.paper);
+		this.createMinTickers(this.paper);
+		this.CreateMajTickers(this.paper);
+		this.createSpinner(this.paper);
+		this.createPointer(this.paper);
+	}
+
+
+	this.setValue = function(data){
 		if(data){
-
 			this.value = 0;
-			for (i=0; i < data.length(); i++){
+			for (i=0; i < data.length; i++){
 				this.value += data[i];
 			}
-			this.value = this.value/data.length();
+			this.value = this.value/data.length;
 		}
 		else
 			this.value = this.defaultValue;
@@ -219,30 +219,13 @@ function Gauge(){
 	
 	this.draw = function(data){
 		var priorValue = this.value;
-		setValue(data);
+		this.setValue(data);
 		var rotAngle = (this.value - priorValue) * 240 / this.range;
 		var rotString = "r" + String(rotAngle) + "," + String(this.cx) + "," + String(this.cy);
-		this.pointer.animate({transform : rotString, this.transDur});
+		this.pointer.animate({transform : rotString}, this.transDur);
 	}
+
+	this.init();
 }
 	
 
-	// marker.attr("fill", "#FF4000");
-	
-	// var marker_circle = paper.circle(100, 100, 6);
-	// marker_circle.attr("fill", "#FF4000");
-	// marker_circle.attr("fill", "#FF4000");
-
-
-
-
-	// function rotationstring(value, center_x,center_y){
-	// 	var dm_ratio = 360.0/300.0; 
-	// 	var rotation_value = value*dm_ratio;
-	// 	var rotation_angle = String(rotation_value);
-	// 	return "r" + rotation_angle + "," + String(center_x) + "," + String(center_y); 
-	// 	}
-
-
-	// marker.animate({transform:rotationstring(200, 100, 100)},3000);
-	// 
